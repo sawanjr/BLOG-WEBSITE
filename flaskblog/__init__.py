cd51e2a9876b239ece12c1a7017122682b9a53bd
SawanRawatJr <sawanrawatjr10@gmail.com>
2023-12-31 14:15:15 +0530

    [updated]

--- flaskblog/__init__.py
+++ flaskblog/__init__.py
@@ -3,7 +3,10 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_bcrypt import Bcrypt
 from flask_login import LoginManager
 
-app = Flask(__name__)
+app = Flask(__name__) # the app is  defined here
+
+
+
 app.config['SECRET_KEY'] = 'sawanjrkey'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
 db = SQLAlchemy(app)
--- flaskblog/models.py
+++ flaskblog/models.py
@@ -8,7 +8,7 @@ def load_user(user_id):
     return User.query.get(int(user_id))
 
 
-class User(db.Model, UserMixin):
+class User(db.Model, UserMixin): # UserMixin provides some methods  , This includes methods like is_authenticated, is_active, is_anonymous, and properties like get_id
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(20), unique=True, nullable=False)
     email = db.Column(db.String(120), unique=True, nullable=False)
@@ -16,7 +16,7 @@ class User(db.Model, UserMixin):
     password = db.Column(db.String(60), nullable=False)
     posts = db.relationship('Post', backref='author', lazy=True)
 
-    def __repr__(self):
+    def __repr__(self): # Defines how objects are represented as strings
         return f"User('{self.username}', '{self.email}', '{self.image_file}')"
 
 
