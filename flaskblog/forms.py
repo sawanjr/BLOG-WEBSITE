e6074e7f069a5e30be61bab2aef7eada60671099
SawanRawatJr <sawanrawatjr10@gmail.com>
2024-01-03 22:03:19 +0530

    Added emaiail features

--- flaskblog/forms.py
+++ flaskblog/forms.py
@@ -61,3 +61,21 @@ class PostForm(FlaskForm):
     title  = StringField( 'title' , validators=[DataRequired()])
     content = TextAreaField('content' , validators=[DataRequired()])
     submit = SubmitField('post')
+
+
+
+class RequestResetForm(FlaskForm):
+     email = StringField('Email', validators=[DataRequired(), Email()])
+     submit = SubmitField('Request password Reset')
+
+     def validate_email(self, email):
+        user = User.query.filter_by(email=email.data).first()
+        if user is None:
+            raise ValidationError('There is no account with this email . You must register first.')
+
+
+class ResetPasswordForm(FlaskForm):
+     password = PasswordField('Password', validators=[DataRequired()])
+     confirm_password = PasswordField('Confirm Password',validators=[DataRequired(), EqualTo('password')])
+     submit = SubmitField('Reset Password')
+     
